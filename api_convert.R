830f025638c66899586afd7c0d6e1097c5dd9a7e
Dadmehr <59148627+dadmehrdi@users.noreply.github.com>
2024-03-27 09:29:08 -0700

    Add files via upload

--- api_convert.R
+++ api_convert.R
@@ -1,7 +1,15 @@
+install.packages("readr") # Install readr package
+install.packages("httr") # Install httr package
+install.packages("jsonlite") # Install jsonlite package
+
 library(httr)
 library(jsonlite)
+library(readr) # Load the readr package
 
-# Your existing dataframe
+# Use this to load your data
+df <- read.csv("path/to/your/file.csv")
+
+# Instead I use this sample dataframe
 df <- data.frame(
   ID = c("R1", "R2", "R3", "R4", "R5"),
   IP = c("204.112.85.145", "83.77.117.59", "191.181.189.173", "203.95.118.150", "34.41.170.141")
@@ -19,8 +27,14 @@ fetch_location_data <- function(ip, api_key) {
   )
 }
 
+# Visit ipinfo.io
+# Sign up for an account or log in if you already have one.
+# Once logged in, navigate to the API section or your account settings to find your API key.
+# Copy the API key (Access Token on Token tab) and replace "your_api_key_here" in the script with your copied key.
+
 # Replace 'your_api_key_here' with your actual ipinfo.io API key
-api_key <- "d0065d6605c80f"
+# I ran the code below with my api key from my account
+api_key <- "your_api_key_here"
 
 # Apply the function to each IP address in the dataframe and collect results
 locations <- lapply(df$IP, fetch_location_data, api_key = api_key)
